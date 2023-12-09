1ab96c038ee5bb8db252c4caacf4dc6a47764985
Timur Kim <kimutir@gmail.com>
2023-12-09 15:56:27 +1100

    1.7.2

--- src/main/java/com/amvera/cli/client/KeycloakAuthClient.java
+++ src/main/java/com/amvera/cli/client/KeycloakAuthClient.java
@@ -1,12 +1,12 @@
 package com.amvera.cli.client;
 
+import com.amvera.cli.dto.auth.AuthRequest;
 import com.amvera.cli.dto.auth.AuthResponse;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.PostExchange;
 
 public interface KeycloakAuthClient {
-
-//    @GetExchange
-    @PostExchange(url = "/auth/realms/amvera/protocol/openid-connect/token", contentType = "")
-    AuthResponse login();
+    @PostExchange(url = "/auth/realms/amvera/protocol/openid-connect/token", contentType = "application/x-www-form-urlencoded")
+    AuthResponse login(@RequestBody AuthRequest body);
 
 }
